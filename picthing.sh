edf8dab19540fa46132239bab679cefc257fee10
sethunya <saulr18@students.ecu.edu>
2019-01-24 12:09:01 -0500

    Improved readability (probably)

--- picthing.sh
+++ picthing.sh
@@ -4,123 +4,117 @@
 
 # variable reference
 col=0            # column count
-prow=0           # row count
-maxrow=0	     # height of image
-filename=""	     # location of image
-rgba=""			 # image transparency indicator
-seethru=""	     # pixel transparency indicator
-pixelco=""	     # rgb value of pixel color
-filecheck=""	 # uses magick to check for the file
-filter="point"   # image filtering (set to point by default)
+curRow=0           # row count
+maxrow=0         # height of image
+filename=""      # location of image
+tempfile=""      # location of the temporary image
+rgba="n"         # image transparency indicator
+seethru=""       # pixel transparency indicator
+pixelclr=""      # rgb value of pixel color
+filecheck=""     # uses magick to check for the file
 
-# creates the picture
-addline() {
-        # loops until prow reaches maxrow
-        while true; do
-
-			# transparency defaults to n
-			seethru="n"
-
-			# reads pixel color information
-			pixelco=$(convert $filename -colorspace RGB -format '%[pixel:p{'$col','$prow'}]' info:-)
-			
-			# checks for image transparency
-			if [ "$rgba" = 'y' ]; then
-				# checks for pixel transparency
-				if [ "$pixelco" = "rgba(0,0,0,0)" ]; then
-					seethru="y"
-				else
-				# alters rgb output to suit printf
-				pixelco=$(echo "$pixelco" | cut -c 6- | rev | cut -c 2- | rev | tr ',' ';')
-				pixelco=$(echo "$pixelco" | awk -F ';' '{print $1 ";" $2 ";" $3}')
-				fi
-			else
-				# alters rgb output to suit printf
-				pixelco=$(echo "$pixelco" | cut -c 5- | rev | cut -c 2- | rev | tr ',' ';')
-			fi
-
-	        # creates pixel from picture information
-			if [ "$seethru" = "y" ]; then
-				# prints nothing to simulate transparency
-				printf "  "
-			else
-				# prints a block of whatever color it was given
-				printf "\x1b[38;2;${pixelco}m\u25A0 \x1b[0m"
-			fi
-
-			#increases column count
-	        col=$((col + 1))
-
-	        # determines whether the row should end
-	        if [ "$col" -eq "$width" ]; then
-	        	# resets column count and increases row count
-				col=0
-	            prow=$((prow + 1))
-	            printf "\\n"
-	                             
-			    # chooses between exiting and looping
-	            if [ "$prow" -eq "$maxrow" ]; then
-					# complements the user and removes the temporary file
-	                printf "Truly a masterpeice!\\n"
-					rm $filename
-	                    exit
-	            fi
-	        fi
-        done
+# determines whether or not the row is complete
+CheckSize()
+{
+  if [ "$col" -eq "$width" ]; then
+    # resets column count and increases row count
+    col=0
+    curRow=$((curRow + 1))
+    printf "\\n"
+                                 
+    if [ "$curRow" -eq "$maxrow" ]; then
+      # complements the user and removes the temporary file
+      printf "Truly a masterpeice!\\n"
+      rm $tempfile
+      exit
+    fi
+  fi
+  PrintRow
 }
 
-# requests filename
-while true; do
-	read -r -p "Enter the location of your image: " filename
-	filecheck=$(identify "$filename" 2> /dev/null)
-	if [ "$filecheck" = "" ]; then
-		printf "Invalid file.\\n"
-	else
-		break
-	fi
-done
+# does it what it says
+PrintRow() 
+{
+  pixelclr=$(convert $tempfile -colorspace RGB -format '%[pixel:p{'$col','$curRow'}]' info:-)
 
-# requests width
-while true; do
-	read -r -p "Enter the width of the output (32 or 64 is recommended): " width
-	if [[ "$width" =~ ^[0-9]+$ ]]; then
-		break
-	else
-		printf "Enter a number.\\n"	
-	fi
-done
+  # checks for image transparency
+  if [ "$rgba" = 'y' ]; then
+    # checks for pixel transparency
+    if [ "$pixelclr" = "rgba(0,0,0,0)" ]; then
+      # prints space to simulate transparency
+      printf "  "
+    else
+      # alters rgb output to suit printf and prints a block
+      pixelclr=$(echo "$pixelclr" | cut -c 6- | rev | cut -c 2- | rev | tr ',' ';')
+      pixelclr=$(echo "$pixelclr" | awk -F ';' '{print $1 ";" $2 ";" $3}')
+      printf "\x1b[38;2;${pixelclr}m\u25A0 \x1b[0m"
+    fi
+  else
+    # alters rgb output to suit printf and prints a block
+    pixelclr=$(echo "$pixelclr" | cut -c 5- | rev | cut -c 2- | rev | tr ',' ';')
+    printf "\x1b[38;2;${pixelclr}m\u25A0 \x1b[0m"
+  fi
 
-# requests resampling type - commented because I don't think most people care about this
-#
-#	while true; do
-#		read -p "Pick a resampling filter. (point/box/triangle): " filter
-#		case $filter in
-#			point )
-#				break;;
-#			box )
-#				break;;
-#    	    	    triangle )
-#				break;;
-#			*)
-#				printf "Invalid entry.\n"
-#		esac
-#	done
-#	printf "\n"
+  col=$((col + 1))
+  CheckSize
+}
+
+# determines whether or not the image contains transparency information
+CheckTransp()
+{
+  rgba=$(convert $tempfile -colorspace RGB -format '%[pixel:p{'1','1'}]' info:- | tr -cd , | wc -c)
+  if [ "$rgba" -eq 3 ]; then
+    rgba="y"
+  else
+    rgba="n"
+  fi
+}
 
 # creates a temporary (usually smaller) version of the image
-convert "$filename"[0] -filter $filter -resize "$width" rsz.png
-filename=rsz.png
+MakeTemp()
+{
+  convert "$filename"[0] -filter point -resize "$width" rsz.png
+  tempfile=rsz.png
+}
 
-# sets the max number of rows according to image height
-maxrow=$(convert $filename -format "%h" info:-)
+# defines maximum width of output
+GetWidth()
+{
+  while true; do
+    read -r -p "Enter the width of the output (32 / 64 are recommended): " width
+    if [[ "$width" =~ ^[0-9]+$ ]]; then
+      break
+    else
+      printf "Enter a number.\\n" 
+    fi
+  done
+}
 
-# determines whether or not the image contains transparency
-rgba=$(convert $filename -colorspace RGB -format '%[pixel:p{'1','1'}]' info:- | tr -cd , | wc -c)
-if [ "$rgba" -eq 3 ]; then
-	rgba="y"
-else
-	rgba="n"
-fi
+# defines source image
+GetFile()
+{
+  while true; do
+    read -r -p "Enter the location of your image: " filename
+    filecheck=
+    if [ "$(identify "$filename" 2> /dev/null)" = "" ]; then
+      printf "Invalid file.\\n"
+    else
+      break
+    fi
+  done
+}
 
-# enters that big loop
-addline
+Commence()
+{
+  GetFile
+  GetWidth
+  MakeTemp
+  CheckTransp
+
+  # sets maxrow according to height of the temporary file
+  maxrow=$(convert $tempfile -format "%h" info:-)
+
+  PrintRow
+}
+
+Commence
